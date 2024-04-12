69a77fb6764600a2cb8b0ee0224089d789b03a20
Chakara123 <chakkaravarthy.murugasen@effem.com>
2024-04-12 22:44:36 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,46 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin-next');
 
 module.exports = {
-    entry: "./src/index.js",
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'index.js'),
     output: {
-        // options related to how webpack emits results
-        filename: '[name]/index.js',
-        chunkFilename: 'chunks/[name].[chunkhash].chunk.js',
-        // This option determines the name of each output bundle.
-        // The bundle is written to the directory specified by the output.path option.
-        //library: libraryName,
-        // The variable 'library' will be bound with the return value of your entry file,
-        // if the resulting output is included as a script tag in an HTML page.
-        //libraryTarget: 'umd',
-        // Configure how the library will be exposed.
-        // libraryTarget: 'umd' - This exposes your library under all the module definitions,
-        // allowing it to work with CommonJS, AMD and as global variable.
-        //umdNamedDefine: true,
-        // When using libraryTarget: "umd", setting output.umdNamedDefine to true will name the AMD module of the UMD build.
-        // Otherwise an anonymous define is used.
-        path: path.resolve(__dirname, 'dist'),
-        // the target directory for all output files
-        // must be an absolute path (use the Node.js path module)
-        publicPath: '/'
-        // the url to the output directory resolved relative to the HTML page
-      },
-    plugins: [
-        new HtmlWebpackPlugin({
-            //hash: false,
-            filename: './index.html',
-            inlineSource: '.(js|css)$'
-        }),
-        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
-    ],
-    module: {
-        rules: [
-            {
-                test: /.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                loader: "babel-loader",
-                options:{
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-                },
-                },
-            },
-        ],
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
     },
-};
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+              ],
+            },
+          },
+        },
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          use: {
+            loader: 'file-loader',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+    ],
+    devServer: {
+      port: '3000',
+      historyApiFallback: true,
+    },
+  };
