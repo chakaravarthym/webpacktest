e87befbf06dd518fde4d61f2f9202e91570f4e84
Chakara123 <chakkaravarthy.murugasen@effem.com>
2024-04-14 21:39:21 +0530

    webpack

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import img1 from './hello.jpeg';
 export default function App() {
   return (
     <div>
-      <h1>React App</h1>
+      <h1>React App webpack</h1>
       <div>
         <img src={img1} />
       </div>
